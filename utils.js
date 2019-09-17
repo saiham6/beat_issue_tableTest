41100bc2c505ef7865f3f8df2efcde235364d316
Saiham Rahman <saiham.unknown@gmail.com>
2019-09-18 02:32:35 +0600

    refractor

--- utils.js
+++ utils.js
@@ -1,6 +1,10 @@
 class Utils {
   constructor() {
   }
+  /**
+   * 
+   * @param {Promise} file 
+   */
   async loadJSON(file) {
       const resp = await fetch(file);
       const jsonData = await resp.json();
