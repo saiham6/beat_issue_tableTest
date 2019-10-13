039e5ae216c34e90a4a6721176f72a2c7b70cb47
saiham6 <saiham.unknown@gmail.com>
2019-10-13 12:14:01 +0600

    code allignment

--- issues.js
+++ issues.js
@@ -4,7 +4,7 @@ class Issues {
     this.util = new Utils();
   }
   /**
-   * *This will be used for adding the issue to the Local VM Storage.
+   * This will be used for adding the issue to the Local VM Storage.
    * @param {Object} issue Will pass the created Object
    */
   add(issue) {
@@ -14,7 +14,7 @@ class Issues {
     
   }
   /**
-   * *This method will write the updated JSON file in session VM to the local file.
+   * This method will write the updated JSON file in session VM to the local file.
    * @param {Object} jsonObj The updated JSON file.
    * @param {JSON} file      The file to be written to.
    */
@@ -22,7 +22,7 @@ class Issues {
     
   }
   /**
-   ** Will update the current json file with the new values
+   * Will update the current json file with the new values
    * @param {Object} file The JSON file to be sent
    * @return {void}
    */
@@ -40,14 +40,14 @@ class Issues {
 
   }
   /**
-   ** Will return all the objects in the JSON file
+   * Will return all the objects in the JSON file
    * @return {Object} The whole JSON file as an Object file.
    */
   getAll() {
     return this.issues;
   }
   /**
-   * *This will read the JSON file
+   * This will read the JSON file
    * @param {JSON} file The file to be read
    */
   async getIssuesFromFile(file) {
